2f0651b3015af6953b6ba8bca66c594d63d7fa8c
Simen Bekkhus <sbekkhus91@gmail.com>
2022-08-17 23:25:22 +0200

    chore: update jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     // Automatically clear mock calls and instances between every test
     clearMocks: true,
     roots: ["<rootDir>/src/"],
+    testEnvironment: "jsdom",
     watchPlugins: [
         "jest-watch-typeahead/filename",
         "jest-watch-typeahead/testname",
