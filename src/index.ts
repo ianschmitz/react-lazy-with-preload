c7e667bb91b63d20609352d5c75f622be520ced5
Eric Parsons <eric.d.parsons@gmail.com>
2022-08-22 10:28:37 -0600

    fix: prevent remount of underlying component (#22)
    Co-authored-by: Simen Bekkhus <sbekkhus91@gmail.com>
    Co-authored-by: Eric Parsons <eric.parsons@vertigis.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ComponentType, createElement, forwardRef, lazy } from "react";
+import { ComponentType, createElement, forwardRef, lazy, useRef } from "react";
 
 export type PreloadableComponent<T extends ComponentType<any>> = T & {
     preload: () => Promise<T>;
@@ -7,29 +7,37 @@ export type PreloadableComponent<T extends ComponentType<any>> = T & {
 export function lazyWithPreload<T extends ComponentType<any>>(
     factory: () => Promise<{ default: T }>
 ): PreloadableComponent<T> {
-    const LazyComponent = lazy(factory);
+    const ReactLazyComponent = lazy(factory);
+    let PreloadedComponent: T | undefined;
     let factoryPromise: Promise<T> | undefined;
-    let LoadedComponent: T | undefined;
 
     const Component = forwardRef(function LazyWithPreload(props, ref) {
+        // Once one of these is chosen, we must ensure that it continues to be
+        // used for all subsequent renders, otherwise it can cause the
+        // underlying component to be unmounted and remounted.
+        const ComponentToRender = useRef(
+            PreloadedComponent ?? ReactLazyComponent
+        );
         return createElement(
-            LoadedComponent ?? LazyComponent,
+            ComponentToRender.current,
             Object.assign(ref ? { ref } : {}, props) as any
         );
-    }) as any as PreloadableComponent<T>;
+    });
 
-    Component.preload = () => {
+    const LazyWithPreload = Component as any as PreloadableComponent<T>;
+
+    LazyWithPreload.preload = () => {
         if (!factoryPromise) {
             factoryPromise = factory().then((module) => {
-                LoadedComponent = module.default;
-                return LoadedComponent;
+                PreloadedComponent = module.default;
+                return PreloadedComponent;
             });
         }
 
         return factoryPromise;
     };
 
-    return Component;
+    return LazyWithPreload;
 }
 
 export default lazyWithPreload;
